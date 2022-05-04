b7142a76e14f8b91ff40036f31b50b60b0acee79
richardljohn <richardljohn@gmail.com>
2022-05-04 13:53:24 -0400

    Mision Complete.

---
+++ Homework-03/src/app/places/discover/discover.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { SegmentChangeEventDetail } from '@ionic/core';
+
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
+
+@Component({
+  selector: 'app-discover',
+  templateUrl: './discover.page.html',
+  styleUrls: ['./discover.page.scss']
+})
+export class DiscoverPage implements OnInit {
+  loadedPlaces: Place[];
+  theFirstItem: Place[];
+
+  constructor(
+    private placesService: PlacesService,
+    private menuCtrl: MenuController
+  ) {}
+
+  ngOnInit() {
+    this.loadedPlaces = this.placesService.places;
+    this.theFirstItem = this.loadedPlaces.slice(1);
+  }
+
+  onOpenMenu() {
+    this.menuCtrl.toggle();
+  }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
+  }
+}
